253e4af46c3f2392442703bc4e396a8247b07824
Elías Heresi <elias.heresi@gmail.com>
2022-04-24 18:52:44 +0200

    Added background image and containers for cards

--- public/js/components/AppComponent.js
+++ public/js/components/AppComponent.js
@@ -10,7 +10,7 @@ class AppComponent extends Component {
 
   render() {
     this.element.innerHTML = `<header>
-      <ul>
+      <ul class="list-unstyled">
         <a href="index.html">Main page</a>
         <a href="local.html">My pokemon</a>
       </ul>
@@ -18,7 +18,9 @@ class AppComponent extends Component {
       <main class="basic-container">
         <section class="page-scroller__lower">
         </section>
-        <ul class="pokemon-list"></ul>
+        <div class="container px-4">
+          <ul class="pokemon-list row list-unstyled gx-5"></ul>
+        </div>
         <section class="page-scroller__upper">
         </section>
       </main>
--- public/js/components/ChangerComponent.js
+++ public/js/components/ChangerComponent.js
@@ -3,13 +3,24 @@ import Component from "./Component.js";
 class ChangerComponent extends Component {
   page;
 
-  constructor(parentElement, upperOrLower) {
+  constructor(parentElement, upperOrLower, action) {
     super(parentElement, "section", `page-scroller__${upperOrLower}`);
     this.render();
+    this.addListeners();
+    this.action = action;
+    this.upperOrLower = upperOrLower;
   }
 
   render() {
-    this.element.innerHTML = `<i class="fa-solid fa-circle-arrow-left"></i>
-    <i class="fa-solid fa-circle-arrow-right"></i>`;
+    this.element.innerHTML = `<i class="fa-solid fa-circle-arrow-left changer__${this.upperOrLower}--previous"></i>
+    <i class="fa-solid fa-circle-arrow-right changer__${this.upperOrLower}--next"></i>`;
+  }
+
+  addListeners() {
+    this.element
+      .querySelector("i.changer__next")
+      .addEventListener("click", this.action);
   }
 }
+
+export default ChangerComponent;
